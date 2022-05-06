06c902355665f38be4b065297d63a47165302913
Andrey Batasov <andrey.batasov@epfl.ch>
2022-05-06 15:28:04 +0200

    Added /web using Create React App

---
+++ web/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>If not alcohol, what makes you happy? Perhaps, React?</h1>
+    </div>
+  );
+}
+
+export default App;
