ac640bab2eddca5054d2876c6bdbfadd173a552a
Andrey Batasov <andrey.batasov@epfl.ch>
2022-05-29 13:50:36 +0200

    Changed named of data csv files

--- web/src/App.js
+++ web/src/App.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import './App.css';
-import {csv} from "d3";
+import { csv } from "d3";
 import oh_dist from './Assets/images/oh_dist.png';
 import happy_dist from './Assets//images/happy_dist.png';
 import scatter from './Assets/images/scatter.png';
@@ -8,35 +8,37 @@ import radar from './Assets/images/radar.png';
 import useInterval from './Utils/useInterval';
 import AnimatedBarChart from './Components/BarChart/AnimatedBarChart';
 import WorldMap from './Components/WorldMap/WorldMap';
-import {map_data_2005, map_data_2021} from './Assets/data'
-import {data_2005, data_2006, data_2007, data_2008, data_2009, 
-        data_2010, data_2011, data_2012, data_2013, data_2014, 
-        data_2015, data_2016, data_2017, data_2018, data_2019, 
-        data_2020, data_2021} from './Assets/data'
+import { map_data_2005, map_data_2021 } from './Assets/data'
+import {
+  data_2005, data_2006, data_2007, data_2008, data_2009,
+  data_2010, data_2011, data_2012, data_2013, data_2014,
+  data_2015, data_2016, data_2017, data_2018, data_2019,
+  data_2020, data_2021
+} from './Assets/data'
 
 
 function App() {
   const [loading, setLoading] = useState(true);
   const [iteration, setIteration] = useState(1)
   const [start, setStart] = useState(true)
-  const [allCsvFiles] = useState([data_2005, data_2006, data_2007, data_2008, data_2009, 
-                                  data_2010, data_2011, data_2012, data_2013, data_2014, 
-                                  data_2015, data_2016, data_2017, data_2018, data_2019, 
-                                  data_2020, data_2021])
-  const [property, setProperty] = useState("pop_est")  
+  const [allCsvFiles] = useState([data_2005, data_2006, data_2007, data_2008, data_2009,
+    data_2010, data_2011, data_2012, data_2013, data_2014,
+    data_2015, data_2016, data_2017, data_2018, data_2019,
+    data_2020, data_2021])
+  const [property, setProperty] = useState("pop_est")
   const [countryData, setCountryData] = useState([])
 
   useEffect(() => {
-   csv(allCsvFiles[iteration]).then((data) => {
-     data.sort((a, b) => b['Life Ladder'] - a['Life Ladder']);
-     setCountryData(data.slice(0, 10));
-     setLoading(false);
-   })
-  },[iteration, allCsvFiles]);
-  
+    csv(allCsvFiles[iteration]).then((data) => {
+      data.sort((a, b) => b['Life Ladder'] - a['Life Ladder']);
+      setCountryData(data.slice(0, 10));
+      setLoading(false);
+    })
+  }, [iteration, allCsvFiles]);
+
   // update the year each 8 seconds
   useInterval(() => {
-    if(start) {
+    if (start) {
       const nextIter = (iteration + 1) % allCsvFiles.length
       setIteration(nextIter)
     }
@@ -47,11 +49,11 @@ function App() {
       <div class="container">
         <div class="page page-1">
           <h1 class="page-1-title">Which countries are the happiest?</h1>
-          <div class="page-1-chart"> {!loading && <AnimatedBarChart data={countryData}/>} {loading && <div>Loading...</div>}</div>
-          <button  onClick={ () => setStart(!start)} >
-        {start ? "Pause" : "Start"}
-      </button>
-          <h1 class="page-1-text">{2005 + (iteration - 1) % allCsvFiles.length }</h1>
+          <div class="page-1-chart"> {!loading && <AnimatedBarChart data={countryData} />} {loading && <div>Loading...</div>}</div>
+          <button onClick={() => setStart(!start)} >
+            {start ? "Pause" : "Start"}
+          </button>
+          <h1 class="page-1-text">{2005 + (iteration - 1) % allCsvFiles.length}</h1>
         </div>
         <div class="page page-2">
           <div class="page-2-title">Explore</div>
