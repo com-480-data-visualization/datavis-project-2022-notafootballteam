53c5c95ca32ca8b2e1aa8f040cb4d2799b87022b
Andrey Batasov <andrey.batasov@epfl.ch>
2022-06-03 05:17:58 +0200

    Started integrating text from milestone 3 doc, improved styles

--- web/src/Components/Pages/Page1.jsx
+++ web/src/Components/Pages/Page1.jsx
@@ -6,21 +6,34 @@ import PlayButton from '../PlayButton/PlayButton';
 import AnimatedBarChart from '../../Components/BarChart/AnimatedBarChart';
 
 export default function Page1(props) {
+
+  const textTop = `Let’s start our journey to happiness by a selfish but essential issue: where should I build my future family?
+  In the age of digital nomads, with a good degree in your pocket (or simply some advanced programming skills) the world is open to you, so feel free to choose.
+  We provide you with a light but insightful visualization of your options and their evolution in time.
+  If you were lucky enough to already live in one of them, we’ll invite you to follow its path through time.`;
+
+  const textBottom = `That is the total number of countries who have been among the top 10 happiest, from the existence of the happiness index to our days.
+  So if you notice a huge similarity in the colors shown between 2005 and 2021, that is perfectly normal.
+  An expert observer will probably also note the following : the financial crisis reflects perfectly on the most abrupt changes of this ranking.
+  The years directly following 2007 will contain the most exotic results while we slowly go back to “normal”. Venezuela, Mexico and Costa Rica had the chance to appear, and to disappear almost as fast.
+  No other “South” country (in economical terms) has ever appeared.`;
+
   return (
     <Box id='box-1'>
       <h1 className="page-1-title">{"WHAT MAKES COUNTRIES HAPPY?"}</h1>
-      <p className="page-1-text-top-left">{"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sagittis interdum ullamcorper. Aenean quis imperdiet nunc. Nulla vitae imperdiet tellus. Suspendisse ut iaculis sem, id viverra tortor. Nullam a urna vitae justo luctus venenatis. Nullam a aliquet arcu. Suspendisse potenti. Sed quis tortor vitae libero molestie sollicitudin eget congue nisl. Aenean convallis a lectus sed dictum. Curabitur diam odio, rutrum ac interdum quis, congue in sem."}</p>
+      <p className="page-1-text-top-left">{textTop}</p>
       <div className="did-you-know">
-        <p id='did-you-know-text'>Did you know?</p>
-        <p>This will contain info about the yellow color.</p>
+        <p id='did-you-know-text'>🟡 Did you know?</p>
+        <p>Yellow is the color of <a href='https://www.science.org/content/article/what-makes-people-happy-when-skies-are-gray-color-yellow#:~:text=Yellow%20is%20usually%20the%20color%20of%20happy%2C%20joyful%20emotions.'>happiness</a> in many cultures.
+          Happiness seems to be associated with sunny weather in numerous non-desertic countries.</p>
       </div>
       <h2 className="page-1-chart-title">{"Ranking by Happiness Score, " + props.selectedYear}</h2>
       <div className="page-1-chart">
         {props.loading && <div>Loading...</div>}
-        {!props.loading && <AnimatedBarChart data={props.data}/>}
+        {!props.loading && <AnimatedBarChart data={props.data} />}
       </div>
       <PlayButton playTime={props.playTime} handleClick={() => props.setPlayTime(!props.playTime)} />
-      <p className="page-1-text-bottom">{"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla mattis tempor tellus vitae placerat. Integer cursus nibh ex, a convallis neque venenatis ac. Praesent quam magna, auctor at consectetur bibendum, lobortis at tellus. Donec faucibus eget ligula eu pretium. Nam sed volutpat orci. Nulla turpis odio, posuere et imperdiet id, varius sit amet ligula. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Etiam nec consectetur justo. Vestibulum hendrerit, mauris in mattis pretium, nunc lectus tempor ex, eget vehicula dui dui id nunc. Proin vitae lacinia lectus. Morbi luctus ultricies ligula. Quisque semper augue enim, sit."}</p>
+      <p className="page-1-text-bottom">{textBottom}</p>
     </Box>
   );
 };
